6e40f26946d7628011145f03208739e7c9d1bcbb
Veera Kansikas <veera.kansikas@student.tut.fi>
2017-11-08 23:54:54 +0200

    playerparent does everything but turning
    capsule does that

--- controller/Assets/capsuleController.cs
+++ controller/Assets/capsuleController.cs
@@ -7,17 +7,36 @@ public class capsuleController : MonoBehaviour {
   public float rotationspeedY = 40;
   public float rotationspeedZ = 0.005F;
 
-  private Camera cam;
+  public bool isFlying;
+
+  private playerController parent;
+
 
   //initialization
   void Start() {
-    cam = Camera.main;
+    parent = transform.parent.GetComponent<playerController>();
+    //parent.isFlying;
   }
 
+
+  void OnCollisionEnter(Collision col) {
+    parent.isFlying = false;
+  }
+
+
   void FixedUpdate() {
+    if (parent.isFlying) {
+      float yVel = parent.rb.velocity.y + Physics.gravity.y;
+      parent.rb.AddForce(0, -yVel, 0, ForceMode.Acceleration + 1);
+    }
+    else if (Input.GetButtonDown(parent.buttLaunch)) {
+      parent.rb.AddForce(Vector3.up * 500, ForceMode.Acceleration);
+      parent.isFlying = true;
+    }
+
     // rotate
     if (Input.GetAxis("Vertical") != 0) {
-      Vector3 camForward = cam.transform.forward;
+      Vector3 camForward = parent.cam.transform.forward;
       Quaternion camRotation = Quaternion.LookRotation(camForward);
       transform.rotation = Quaternion.Slerp(transform.rotation, camRotation, Time.deltaTime * 3F);
     }
