2d1cb3ce7f8b1f16b39c00ca8822338aa1b1b2a3
Veera Kansikas <veera.kansikas@student.tut.fi>
2017-11-16 10:06:29 +0200

    added rotating by following gaze to the snow scene

---
+++ owl/Assets/Scripts/FollowGaze/ChildController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChildController : MonoBehaviour {
+
+  public float rotationspeedX = 0.01F;
+  public float rotationspeedY = 40;
+  public float rotationspeedZ = 0.005F;
+
+  private ParentController parent;
+
+
+  //initialization
+  void Start() {
+    parent = transform.parent.GetComponent<ParentController>();
+  }
+
+
+  void OnCollisionEnter(Collision col) {
+    parent.isFlying = false;
+  }
+
+
+  void FixedUpdate() {
+    if (parent.isFlying) {
+      float yVel = parent.rb.velocity.y + Physics.gravity.y;
+      parent.rb.AddForce(0, -yVel, 0, ForceMode.Acceleration + 1);
+    }
+    else if (Input.GetButtonDown(parent.buttLaunch)) {
+      parent.rb.AddForce(Vector3.up * 500, ForceMode.Acceleration);
+      parent.isFlying = true;
+    }
+
+    // rotate
+    if (Input.GetAxis("Vertical") != 0) {
+      Vector3 camForward = parent.cam.transform.forward;
+      Quaternion camRotation = Quaternion.LookRotation(camForward);
+      transform.rotation = Quaternion.Slerp(transform.rotation, camRotation, Time.deltaTime * 3F);
+    }
+  }
+}
---
+++ owl/Assets/Scripts/FollowGaze/ParentController.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParentController : MonoBehaviour {
+
+	public float groundspeed = 5.0F;
+	public float launchspeed = 50.0F;
+	public float airspeed = 10.0F;
+	public float gravity = 20.0F;
+	public float drag = 10.0F;
+
+	public string buttFlap;
+	public string buttLook;
+	public string buttDive;
+	public string buttLaunch;
+
+	public bool isFlying;
+
+	public Rigidbody rb;
+	public Camera cam;
+
+
+	// initialization
+	void Start () {
+		Cursor.lockState = CursorLockMode.Locked;
+    cam = Camera.main;
+    rb = GetComponent<Rigidbody>();
+
+    if (Application.isEditor) {
+      buttFlap = "PS4_L2";
+      buttLook = "PS4_L1";
+      buttLaunch = "PS4_X";
+    } else {
+      buttFlap = "PS4_L1";  // L2
+      buttLook = "PS4_Triangle";  // L1
+      buttLaunch = "PS4_X";
+    }
+  }
+
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (isFlying) {
+
+      // alter speed
+      float fly = (Input.GetAxis("Vertical") + 1) * airspeed * Time.deltaTime;
+      transform.Translate(cam.transform.forward * fly);}
+
+    else {
+      // move
+      if (Input.GetAxis("Vertical") != 0) {
+        float walk = (Input.GetAxis("Vertical") * groundspeed * Time.deltaTime);
+				transform.Translate(cam.transform.forward * walk);
+      }
+    }
+	}
+}
